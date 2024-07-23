b3afa4814150e61f78ade2003284bd011bec20b7
AndreiCherniaev <glscene2010@gmail.com>
2024-07-24 00:00:29 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+### Add crc
+Example how to calc CRC-16/ISO-HDLC, CRC-16/ISO-IEC-14443-3-B, CRC-16/X-25, CRC-B, X-25 using boost C++ library
+```
+git submodule add -b master https://github.com/boostorg/crc
+```
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include "boost/crc.hpp"
+
+typedef boost::crc_optimal<16, 0x1021, 0xffff, 0xffff, true, true> crc_16_X25;
+
+int main(int argc, char *argv[]){
+    const uint8_t arr[3]= {0xAA, 0xBB};
+    crc_16_X25 crc{};
+    crc.process_block(arr, &(arr[sizeof(arr)-1]));
+
+    printf("crc 0x%04lX", crc.checksum());
+}
